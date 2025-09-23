fa9efb8809319e243f2222316f7db42ecf704a0d
Diaxpegu <diaxpegu@gmail.com>
2025-09-23 02:01:10 -0300

    Correccion 3 database.py

--- database.py
+++ database.py
@@ -3,11 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.environ.get("MYSQLUSER", "root")
-DB_PASS = os.environ.get("MYSQLPASSWORD", "")
-DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
-DB_NAME = os.environ.get("MYSQLDATABASE", "railway")
-DB_PORT = int(os.environ.get("MYSQLPORT", 3306))
+DB_USER = os.environ.get("DB_USER", "root")
+DB_PASS = os.environ.get("DB_PASS", "")
+DB_HOST = os.environ.get("DB_HOST", "mysql.railway.internal")
+DB_NAME = os.environ.get("DB_NAME", "railway")
+DB_PORT = os.environ.get("DB_PORT", 3306)
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
