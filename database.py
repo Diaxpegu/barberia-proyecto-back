2b3bb9929f0ef8f811e9d400a6eee5dd9d0d78f8
Diaxpegu <diaxpegu@gmail.com>
2025-09-23 01:53:30 -0300

    Correccion database.py

--- database.py
+++ database.py
@@ -3,14 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.environ.get("MYSQLUSER")
-DB_PASS = os.environ.get("MYSQLPASSWORD")
-DB_HOST = os.environ.get("MYSQLHOST")
-DB_NAME = os.environ.get("MYSQLDATABASE")
-DB_PORT = os.environ.get("MYSQLPORT", "3306")
+DB_USER = os.environ.get("MYSQLUSER", "root")
+DB_PASS = os.environ.get("MYSQLPASSWORD", "")
+DB_HOST = os.environ.get("MYSQLHOST", "mysql.railway.internal")
+DB_NAME = os.environ.get("MYSQLDATABASE", "railway")
+DB_PORT = os.environ.get("MYSQLPORT", 3306)
 
 SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
