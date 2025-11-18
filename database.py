340b00f7197e72d5432f296ae3ebe01bf9cbbab1
aarielhr <aarielhr@gmail.com>
2025-11-18 16:38:14 -0300

    intento n4?

--- database.py
+++ database.py
@@ -7,8 +7,9 @@ import os
 MONGO_URL = os.getenv(
     "MONGO_URL",
     "mongodb://mongo:password@mongodb.railway.internal:27017/test?authSource=admin"
+    
 )
-MONGO_DB = os.getenv("MONGO_DB", "test")
+MONGO_DB = os.getenv("MONGO_DB", "test")  # Nombre de la base de datos
 
 # -----------------------
 # Cliente MongoDB
@@ -29,7 +30,34 @@ barberos_col = db["barberos"]
 servicios_col = db["servicios"]
 productos_col = db["productos"]
 reservas_col = db["reservas"]
+disponibilidades_col = db["disponibilidades"]
+notificaciones_col = db["notificaciones"]
 jefes_col = db["jefes"]
 admin_col = db["admin"]
 
 
+"""from pymongo import MongoClient
+import os
+
+# Variables de entorno de Railway
+MONGO_USER = os.getenv("MONGOUSER", "mongo")
+MONGO_PASS = os.getenv("MONGOPASSWORD", "")
+MONGO_HOST = os.getenv("MONGOHOST", "mongodb.railway.internal")
+MONGO_PORT = os.getenv("MONGOPORT", "27017")
+MONGO_DB = "test"  # Nombre de tu base de datos
+
+# Construir URL de conexión (si no se define directamente en Railway)
+MONGO_URL = os.getenv("MONGO_URL") or f"mongodb://{MONGO_USER}:{MONGO_PASS}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?authSource=admin"
+# Crear cliente y conectar base de datos
+client = MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+# Colecciones existentes
+clientes_col = db["clientes"]
+barberos_col = db["barberos"]
+servicios_col = db["servicios"]
+productos_col = db["productos"]
+reservas_col = db["reservas"]
+disponibilidades_col = db["disponibilidades"]
+notificaciones_col = db["notificaciones"]
+jefes_col = db["jefes"]"""
